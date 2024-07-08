3c867a81bb46dedb7b29dacf1a4e2cb597942280
Ahmed094-u <aomar094@uottawa.ca>
2024-07-08 19:32:42 -0400

    third commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Explore from './Components/Explore';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
